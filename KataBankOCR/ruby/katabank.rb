8d3874fc2d2087fc33df16d9e5036ac2f09787db
Tarcio <tarcio@gmail.com>
2014-03-05 00:22:22 +1100

    First draft of solution working - i.e. reading one line

--- KataBankOCR/ruby/katabank.rb
+++ KataBankOCR/ruby/katabank.rb
@@ -1,24 +1,77 @@
 #!/usr/bin/ruby
 
-#  _  _  _  _  _  _  _  _  _ 
+#  _  _  _  _  _  _  _  _  _
 # | || || || || || || || || |
 # |_||_||_||_||_||_||_||_||_|
-#                            
+#
 
 class FileParser
 
+  ZERO  = " _ | ||_|"
+  ONE   = "     |  |"
+  TWO   = " _  _||_ "
+  THREE = " _  _| _|"
+  FOUR  = "   |_|  |"
+  FIVE  = " _ |_  _|"
+  SIX   = " _ |_ |_|"
+  SEVEN = " _   |  |"
+  EIGHT = " _ |_||_|"
+  NINE  = " _ |_| _|"
+
   def read (file)
-    IO.readlines(file).each { |line|
-      @number_data.push break_lines(line)
-    }
+    @parsed_content = Array.new
+    @raw_content = IO.readlines(file)
+    split_lines(0)
+    identify_digits
+    puts "Converted data > #{@parsed_content}"
   end
 
-  def break_line (line)
-    
+  def split_lines (line_num)
+    if line_num < @raw_content.size
+      @raw_content[line_num] = @raw_content[line_num].chomp!.split(//)
+      split_lines(line_num + 1)
+    end
   end
 
-  def identify_number
+  def identify_digits (row = 0, col_range = 0..2)
+    end_range = col_range.end
+    if (end_range < 28)
+      flatten_digit = extract_digit_block(row, col_range).join
+      case flatten_digit
+      when ZERO
+        @parsed_content.push '0'
+      when ONE
+        @parsed_content.push '1'
+      when TWO
+        @parsed_content.push '2'
+      when THREE
+        @parsed_content.push '3'
+      when FOUR
+        @parsed_content.push '4'
+      when FIVE
+        @parsed_content.push '5'
+      when SIX
+        @parsed_content.push '6'
+      when SEVEN
+        @parsed_content.push '7'
+      when EIGHT
+        @parsed_content.push '8'
+      when NINE
+        @parsed_content.push '9'
+      else
+        @parsed_content.push '?'
+      end
+      identify_digits(row + 4, end_range + 1..end_range + 3)
+    end
+  end
 
+  def extract_digit_block (row, col_range)
+    digit_block = Array.new
+    if (row % 3 == 0)
+      digit_block = @raw_content[row][col_range]
+      digit_block = digit_block + extract_digit_block(row + 1, col_range)
+    end
+    digit_block
   end
 
 end
