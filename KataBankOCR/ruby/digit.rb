d8c90e7546230f7bd8192c5e68fa96fb8e7b4b1f
Tarcio <tarcio@gmail.com>
2014-03-05 11:29:14 +1100

    Now reading multiple lines and populating an array of parsed content

---
+++ KataBankOCR/ruby/digit.rb
@@ -0,0 +1,32 @@
+module KataBank
+  class Digit
+
+    ZERO  = " _ | ||_|"
+    ONE   = "     |  |"
+    TWO   = " _  _||_ "
+    THREE = " _  _| _|"
+    FOUR  = "   |_|  |"
+    FIVE  = " _ |_  _|"
+    SIX   = " _ |_ |_|"
+    SEVEN = " _   |  |"
+    EIGHT = " _ |_||_|"
+    NINE  = " _ |_| _|"
+
+    def Digit.convert(flatten_digit)
+      case flatten_digit
+      when ZERO  then '0'
+      when ONE   then '1'
+      when TWO   then '2'
+      when THREE then '3'
+      when FOUR  then '4'
+      when FIVE  then '5'
+      when SIX   then '6'
+      when SEVEN then '7'
+      when EIGHT then '8'
+      when NINE  then '9'
+      else '?'
+      end
+    end
+
+  end
+end
--- KataBankOCR/ruby/katabank.rb
+++ KataBankOCR/ruby/katabank.rb
@@ -4,77 +4,51 @@
 # | || || || || || || || || |
 # |_||_||_||_||_||_||_||_||_|
 #
+$LOAD_PATH << '.'
 
-class FileParser
+require 'digit'
 
-  ZERO  = " _ | ||_|"
-  ONE   = "     |  |"
-  TWO   = " _  _||_ "
-  THREE = " _  _| _|"
-  FOUR  = "   |_|  |"
-  FIVE  = " _ |_  _|"
-  SIX   = " _ |_ |_|"
-  SEVEN = " _   |  |"
-  EIGHT = " _ |_||_|"
-  NINE  = " _ |_| _|"
+module KataBank
 
-  def read (file)
-    @parsed_content = Array.new
-    @raw_content = IO.readlines(file)
-    split_lines(0)
-    identify_digits
-    puts "Converted data > #{@parsed_content}"
-  end
+  class FileParser
 
-  def split_lines (line_num)
-    if line_num < @raw_content.size
-      @raw_content[line_num] = @raw_content[line_num].chomp!.split(//)
-      split_lines(line_num + 1)
+    def read (file)
+      @raw_content = IO.readlines(file)
+      @parsed_content = Array.new((@raw_content.size/4)+1) { "" }
+      split_lines
+      parse_digits
     end
-  end
 
-  def identify_digits (row = 0, col_range = 0..2)
-    end_range = col_range.end
-    if (end_range < 28)
-      flatten_digit = extract_digit_block(row, col_range).join
-      case flatten_digit
-      when ZERO
-        @parsed_content.push '0'
-      when ONE
-        @parsed_content.push '1'
-      when TWO
-        @parsed_content.push '2'
-      when THREE
-        @parsed_content.push '3'
-      when FOUR
-        @parsed_content.push '4'
-      when FIVE
-        @parsed_content.push '5'
-      when SIX
-        @parsed_content.push '6'
-      when SEVEN
-        @parsed_content.push '7'
-      when EIGHT
-        @parsed_content.push '8'
-      when NINE
-        @parsed_content.push '9'
-      else
-        @parsed_content.push '?'
+    def split_lines (line_num = 0)
+      if line_num < @raw_content.size
+        @raw_content[line_num] = @raw_content[line_num].chomp!.split(//)
+        split_lines(line_num + 1)
       end
-      identify_digits(row + 4, end_range + 1..end_range + 3)
     end
-  end
 
-  def extract_digit_block (row, col_range)
-    digit_block = Array.new
-    if (row % 3 == 0)
-      digit_block = @raw_content[row][col_range]
-      digit_block = digit_block + extract_digit_block(row + 1, col_range)
+    def parse_digits (row = 0, col_range = 0..2)
+      end_range = col_range.end
+      if (end_range < 28 && row < @raw_content.size)
+        flatten_digit = extract_digit_block(row, col_range)
+        @parsed_content[(row / 4)].concat Digit.convert(flatten_digit)
+        parse_digits(row, end_range + 1..end_range + 3)
+      elsif row < @raw_content.size
+        parse_digits(row + 4)
+      end
     end
-    digit_block
+
+    def extract_digit_block (row, col_range, counter = 0)
+      digit_block = ""
+      if (counter < 3)
+        digit_block = @raw_content[row][col_range].join
+        digit_block.concat extract_digit_block(row + 1, col_range, counter + 1)
+      end
+      digit_block
+    end
+
   end
 
 end
 
-parser = FileParser.new
+parser = KataBank::FileParser.new
 parser.read "data.txt"
