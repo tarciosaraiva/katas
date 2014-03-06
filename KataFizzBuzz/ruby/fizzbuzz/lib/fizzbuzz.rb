887b2d9d87cdb44de7bd0c96810a52591efdbcc2
Tarcio <tarcio@gmail.com>
2014-03-07 08:58:46 +1100

    Improving tests and adding formatters for rspec

--- KataFizzBuzz/ruby/fizzbuzz/lib/fizzbuzz.rb
+++ KataFizzBuzz/ruby/fizzbuzz/lib/fizzbuzz.rb
@@ -14,14 +14,12 @@ module Fizzbuzz
     private
 
     def execute(posix = 1)
-      if posix <= 100
-        word = ""
-        unless (posix % 3 != 0) then word.concat "Fizz" end
-        unless (posix % 5 != 0) then word.concat "Buzz" end
-        if word.empty? then word = posix.to_s end
-        @result.push word
-        execute(posix + 1)
-      end
+      word = ""
+      unless (posix % 3 != 0) then word.concat "Fizz" end
+      unless (posix % 5 != 0) then word.concat "Buzz" end
+      if word.empty? then word = posix.to_s end
+      @result.push word
+      execute(posix + 1) unless posix == 100
     end
 
   end
--- KataFizzBuzz/ruby/fizzbuzz/spec/fizzbuzz/fizzbuzz_spec.rb
+++ KataFizzBuzz/ruby/fizzbuzz/spec/fizzbuzz/fizzbuzz_spec.rb
@@ -8,6 +8,22 @@ module Fizzbuzz
       @fb = Fizzbuzz.new
     end
 
+    it "should have 100 entries" do
+      @fb.result.size.should eq(100)
+    end
+
+    it "should have 27 Fizzes" do
+      @fb.result.reject {|e| e.to_str != "Fizz"}.size.should eq(27)
+    end
+
+    it "should have 14 Buzzes" do
+      @fb.result.reject {|e| e.to_str != "Buzz"}.size.should eq(14)
+    end
+
+    it "should have 6 FizzBuzzes" do
+      @fb.result.reject {|e| e.to_str != "FizzBuzz"}.size.should eq(6)
+    end
+
     it "should fizz on 3" do
       @fb.result[2].should eq("Fizz")
     end
